7d8fe17b5b37a14d2b19c5b3df7261d9543dac43
trancanh277 <tranminhcanh.tp@gmail.com>
2025-07-30 21:22:49 +0900

    upload session 14 bai tap 1

---
+++ baitap01.js
@@ -0,0 +1,8 @@
+let person = {
+    name: "Jone Doe",
+    age: 25,
+    job: "Developer"
+};
+console.log("Tên", person.name);
+console.log("Tuổi", person.age);
+console.log("Công việc", person.job);
